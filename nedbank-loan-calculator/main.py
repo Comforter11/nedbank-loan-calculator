8b845a2bceb604d88c339853ba24b14e088b6162
Comforter <charismakweya2@gmail.com>
2020-09-23 11:45:56 +0200

    first commit

---
+++ nedbank-loan-calculator/main.py
@@ -0,0 +1,14 @@
+#Nedbank Loan Calculator
+print("---Welcome To Nedbank Loan Console Calculator---")
+
+loan_amount = int(input("Please enter loan amount: "))
+interest_rate = float(input("Please enter your interest rate without a percentage sign"))
+_number_of_months = 12 #how long it would take you to repay the loan.
+
+total_repayment = loan_amount + (loan_amount * interest_rate)
+monthly_installment = total_repayment / _number_of_months
+
+print('Total Repayment: ', total_repayment)
+print("Monthly Installment:", monthly_installment)
+print("Please go to the nearest Nedbank branch with your bank statement, ID and proof of residence.")
+
